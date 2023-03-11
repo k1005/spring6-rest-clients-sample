77f3940ce709faf9d9a4d47469a3471822443f0f
k1005 <erim1005@gmail.com>
2023-03-11 16:17:31 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/spring6restclients/Spring6RestClientsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring6restclients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6RestClientsApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring6RestClientsApplication>(*args)
+}
